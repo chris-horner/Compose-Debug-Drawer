0b7dbe22f26d6b083ec425b7dd9b3ac7c854f168
Bernat Borrás <bernat.borras@adevinta.com>
2021-02-24 19:02:03 +0100

    Move gradle dependencies to beta01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.0.0-alpha12"
+        kotlinCompilerExtensionVersion = "1.0.0-beta01"
     }
 
     lintOptions {
@@ -51,11 +51,11 @@ dependencies {
 
     implementation("androidx.activity:activity-compose:1.3.0-alpha02")
 
-    implementation("androidx.compose.foundation:foundation:1.0.0-alpha12")
-    implementation("androidx.compose.foundation:foundation-layout:1.0.0-alpha12")
-    implementation("androidx.compose.ui:ui:1.0.0-alpha12")
-    implementation("androidx.compose.material:material:1.0.0-alpha12")
-    implementation("androidx.compose.ui:ui-tooling:1.0.0-alpha12")
+    implementation("androidx.compose.foundation:foundation:1.0.0-beta01")
+    implementation("androidx.compose.foundation:foundation-layout:1.0.0-beta01")
+    implementation("androidx.compose.ui:ui:1.0.0-beta01")
+    implementation("androidx.compose.material:material:1.0.0-beta01")
+    implementation("androidx.compose.ui:ui-tooling:1.0.0-beta01")
 
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
