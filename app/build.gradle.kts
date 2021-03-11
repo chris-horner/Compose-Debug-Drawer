b2bb9c09a494318c92a39e65622e5793ef17e2c3
Bernat Borrás <bernat.borras@adevinta.com>
2021-03-11 12:33:02 +0100

    Update appcompat

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling:1.0.0-beta02")
 
     implementation("androidx.core:core-ktx:1.3.2")
-    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.appcompat:appcompat:1.3.0-beta01")
     implementation("com.google.android.material:material:1.3.0")
 
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.0")
