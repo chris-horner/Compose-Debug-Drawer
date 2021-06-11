6f0c65c6c738d8257455b58593fcee1de55588ef
Bernat Borrás <bernat.borras@adevinta.com>
2021-06-11 08:57:40 +0200

    Fix: Header click

--- drawer-base/src/main/java/com/alorma/drawer_base/DebugDrawerHeader.kt
+++ drawer-base/src/main/java/com/alorma/drawer_base/DebugDrawerHeader.kt
@@ -1,12 +1,23 @@
 package com.alorma.drawer_base
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.ArrowDropUp
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.semantics
@@ -15,6 +26,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun DrawerModuleHeader(
     icon: @Composable (() -> Unit)? = null,
@@ -22,18 +34,16 @@ fun DrawerModuleHeader(
     expandedState: Boolean = false,
     onClick: () -> Unit,
 ) {
-    val semanticModifier = Modifier
-        .clickable(onClick = onClick)
-        .semantics {
-            testTag = "Module header $title"
-        }
     Surface(
         modifier = Modifier
             .fillMaxWidth()
             .height(48.dp)
-            .then(semanticModifier),
+            .semantics {
+                testTag = "Module header $title"
+            },
         color = MaterialTheme.colors.surface,
         contentColor = MaterialTheme.colors.secondary,
+        onClick = onClick
     ) {
         Row(
             modifier = Modifier.padding(8.dp),
